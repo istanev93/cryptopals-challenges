145814da7a766511884ed69ba0e256bfd648744d
istanev93 <istanev93@gmail.com>
2018-07-24 23:01:20 +0100

    reserve space in vector

--- xor.cpp
+++ xor.cpp
@@ -4,6 +4,7 @@ namespace Utility {
 
 std::vector<uint8_t> FixedXor(const uint8_t* arg1, size_t arg1len, const uint8_t* arg2, size_t arg2len) {
     std::vector<uint8_t> result;
+    result.reserve(arg1len);
 
     for (auto i = 0; i < arg1len; i++) {
         result.push_back(arg1[i] ^ arg2[i]);
